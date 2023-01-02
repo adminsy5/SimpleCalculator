5039716afd7449210cbba8d0d198c88d5d71b256
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-02 17:49:55 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ans = New System.Windows.Forms.RichTextBox()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.point = New System.Windows.Forms.Button()
+        Me.back = New System.Windows.Forms.Button()
+        Me.minus = New System.Windows.Forms.Button()
+        Me.nine = New System.Windows.Forms.Button()
+        Me.five = New System.Windows.Forms.Button()
+        Me.two = New System.Windows.Forms.Button()
+        Me.multiply = New System.Windows.Forms.Button()
+        Me.equals = New System.Windows.Forms.Button()
+        Me.six = New System.Windows.Forms.Button()
+        Me.one = New System.Windows.Forms.Button()
+        Me.division = New System.Windows.Forms.Button()
+        Me.reminder = New System.Windows.Forms.Button()
+        Me.seven = New System.Windows.Forms.Button()
+        Me.zero = New System.Windows.Forms.Button()
+        Me.plus = New System.Windows.Forms.Button()
+        Me.eight = New System.Windows.Forms.Button()
+        Me.four = New System.Windows.Forms.Button()
+        Me.three = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ans
+        '
+        Me.ans.Font = New System.Drawing.Font("Segoe UI", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ans.Location = New System.Drawing.Point(38, 43)
+        Me.ans.Name = "ans"
+        Me.ans.Size = New System.Drawing.Size(539, 103)
+        Me.ans.TabIndex = 0
+        Me.ans.Text = ""
+        '
+        'clear
+        '
+        Me.clear.Location = New System.Drawing.Point(38, 164)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(264, 77)
+        Me.clear.TabIndex = 1
+        Me.clear.Text = "C"
+        Me.clear.UseVisualStyleBackColor = True
+        '
+        'point
+        '
+        Me.point.Location = New System.Drawing.Point(317, 164)
+        Me.point.Name = "point"
+        Me.point.Size = New System.Drawing.Size(121, 77)
+        Me.point.TabIndex = 3
+        Me.point.Text = "."
+        Me.point.UseVisualStyleBackColor = True
+        '
+        'back
+        '
+        Me.back.Location = New System.Drawing.Point(456, 164)
+        Me.back.Name = "back"
+        Me.back.Size = New System.Drawing.Size(121, 77)
+        Me.back.TabIndex = 4
+        Me.back.Text = "←"
+        Me.back.UseVisualStyleBackColor = True
+        '
+        'minus
+        '
+        Me.minus.Location = New System.Drawing.Point(456, 353)
+        Me.minus.Name = "minus"
+        Me.minus.Size = New System.Drawing.Size(121, 77)
+        Me.minus.TabIndex = 8
+        Me.minus.Text = "-"
+        Me.minus.UseVisualStyleBackColor = True
+        '
+        'nine
+        '
+        Me.nine.Location = New System.Drawing.Point(317, 353)
+        Me.nine.Name = "nine"
+        Me.nine.Size = New System.Drawing.Size(121, 77)
+        Me.nine.TabIndex = 7
+        Me.nine.Text = "9"
+        Me.nine.UseVisualStyleBackColor = True
+        '
+        'five
+        '
+        Me.five.Location = New System.Drawing.Point(181, 353)
+        Me.five.Name = "five"
+        Me.five.Size = New System.Drawing.Size(121, 77)
+        Me.five.TabIndex = 6
+        Me.five.Text = "5"
+        Me.five.UseVisualStyleBackColor = True
+        '
+        'two
+        '
+        Me.two.Location = New System.Drawing.Point(38, 353)
+        Me.two.Name = "two"
+        Me.two.Size = New System.Drawing.Size(121, 77)
+        Me.two.TabIndex = 5
+        Me.two.Text = "2"
+        Me.two.UseVisualStyleBackColor = True
+        '
+        'multiply
+        '
+        Me.multiply.Location = New System.Drawing.Point(456, 453)
+        Me.multiply.Name = "multiply"
+        Me.multiply.Size = New System.Drawing.Size(121, 77)
+        Me.multiply.TabIndex = 12
+        Me.multiply.Text = "x"
+        Me.multiply.UseVisualStyleBackColor = True
+        '
+        'equals
+        '
+        Me.equals.Location = New System.Drawing.Point(317, 453)
+        Me.equals.Name = "equals"
+        Me.equals.Size = New System.Drawing.Size(121, 77)
+        Me.equals.TabIndex = 11
+        Me.equals.Text = "="
+        Me.equals.UseVisualStyleBackColor = True
+        '
+        'six
+        '
+        Me.six.Location = New System.Drawing.Point(181, 453)
+        Me.six.Name = "six"
+        Me.six.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.six.Size = New System.Drawing.Size(121, 77)
+        Me.six.TabIndex = 10
+        Me.six.Text = "6"
+        Me.six.UseVisualStyleBackColor = True
+        '
+        'one
+        '
+        Me.one.Location = New System.Drawing.Point(38, 453)
+        Me.one.Name = "one"
+        Me.one.Size = New System.Drawing.Size(121, 77)
+        Me.one.TabIndex = 9
+        Me.one.Text = "1"
+        Me.one.UseVisualStyleBackColor = True
+        '
+        'division
+        '
+        Me.division.Location = New System.Drawing.Point(456, 550)
+        Me.division.Name = "division"
+        Me.division.Size = New System.Drawing.Size(121, 77)
+        Me.division.TabIndex = 16
+        Me.division.Text = "/"
+        Me.division.UseVisualStyleBackColor = True
+        '
+        'reminder
+        '
+        Me.reminder.Location = New System.Drawing.Point(317, 550)
+        Me.reminder.Name = "reminder"
+        Me.reminder.Size = New System.Drawing.Size(121, 77)
+        Me.reminder.TabIndex = 15
+        Me.reminder.Text = "%"
+        Me.reminder.UseVisualStyleBackColor = True
+        '
+        'seven
+        '
+        Me.seven.Location = New System.Drawing.Point(181, 550)
+        Me.seven.Name = "seven"
+        Me.seven.Size = New System.Drawing.Size(121, 77)
+        Me.seven.TabIndex = 14
+        Me.seven.Text = "7"
+        Me.seven.UseVisualStyleBackColor = True
+        '
+        'zero
+        '
+        Me.zero.Location = New System.Drawing.Point(38, 550)
+        Me.zero.Name = "zero"
+        Me.zero.Size = New System.Drawing.Size(121, 77)
+        Me.zero.TabIndex = 13
+        Me.zero.Text = "0"
+        Me.zero.UseVisualStyleBackColor = True
+        '
+        'plus
+        '
+        Me.plus.Location = New System.Drawing.Point(456, 258)
+        Me.plus.Name = "plus"
+        Me.plus.Size = New System.Drawing.Size(121, 77)
+        Me.plus.TabIndex = 20
+        Me.plus.Text = "+"
+        Me.plus.UseVisualStyleBackColor = True
+        '
+        'eight
+        '
+        Me.eight.Location = New System.Drawing.Point(317, 258)
+        Me.eight.Name = "eight"
+        Me.eight.Size = New System.Drawing.Size(121, 77)
+        Me.eight.TabIndex = 19
+        Me.eight.Text = "8"
+        Me.eight.UseVisualStyleBackColor = True
+        '
+        'four
+        '
+        Me.four.Location = New System.Drawing.Point(181, 258)
+        Me.four.Name = "four"
+        Me.four.Size = New System.Drawing.Size(121, 77)
+        Me.four.TabIndex = 18
+        Me.four.Text = "4"
+        Me.four.UseVisualStyleBackColor = True
+        '
+        'three
+        '
+        Me.three.Location = New System.Drawing.Point(38, 258)
+        Me.three.Name = "three"
+        Me.three.Size = New System.Drawing.Size(121, 77)
+        Me.three.TabIndex = 17
+        Me.three.Text = "3"
+        Me.three.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(622, 715)
+        Me.Controls.Add(Me.plus)
+        Me.Controls.Add(Me.eight)
+        Me.Controls.Add(Me.four)
+        Me.Controls.Add(Me.three)
+        Me.Controls.Add(Me.division)
+        Me.Controls.Add(Me.reminder)
+        Me.Controls.Add(Me.seven)
+        Me.Controls.Add(Me.zero)
+        Me.Controls.Add(Me.multiply)
+        Me.Controls.Add(Me.equals)
+        Me.Controls.Add(Me.six)
+        Me.Controls.Add(Me.one)
+        Me.Controls.Add(Me.minus)
+        Me.Controls.Add(Me.nine)
+        Me.Controls.Add(Me.five)
+        Me.Controls.Add(Me.two)
+        Me.Controls.Add(Me.back)
+        Me.Controls.Add(Me.point)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.ans)
+        Me.Name = "Form1"
+        Me.Text = "Calculator By piyush"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ans As RichTextBox
+    Friend WithEvents clear As Button
+    Friend WithEvents point As Button
+    Friend WithEvents back As Button
+    Friend WithEvents minus As Button
+    Friend WithEvents nine As Button
+    Friend WithEvents five As Button
+    Friend WithEvents two As Button
+    Friend WithEvents multiply As Button
+    Friend WithEvents equals As Button
+    Friend WithEvents six As Button
+    Friend WithEvents one As Button
+    Friend WithEvents division As Button
+    Friend WithEvents reminder As Button
+    Friend WithEvents seven As Button
+    Friend WithEvents zero As Button
+    Friend WithEvents plus As Button
+    Friend WithEvents eight As Button
+    Friend WithEvents four As Button
+    Friend WithEvents three As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class Form1
+    Dim sign As String
+    Dim value1, value2 As Double
+    Private Sub zero_Click(sender As Object, e As EventArgs) Handles zero.Click
+        ans.Text = ans.Text & 0
+    End Sub
+
+    Private Sub one_Click(sender As Object, e As EventArgs) Handles one.Click
+        ans.Text = ans.Text & 1
+    End Sub
+
+    Private Sub two_Click(sender As Object, e As EventArgs) Handles two.Click
+        ans.Text = ans.Text & 2
+    End Sub
+
+    Private Sub three_Click(sender As Object, e As EventArgs) Handles three.Click
+        ans.Text = ans.Text & 3
+    End Sub
+
+    Private Sub four_Click(sender As Object, e As EventArgs) Handles four.Click
+        ans.Text = ans.Text & 4
+    End Sub
+
+    Private Sub five_Click(sender As Object, e As EventArgs) Handles five.Click
+        ans.Text = ans.Text & 5
+    End Sub
+
+    Private Sub six_Click(sender As Object, e As EventArgs) Handles six.Click
+        ans.Text = ans.Text & 6
+    End Sub
+
+    Private Sub seven_Click(sender As Object, e As EventArgs) Handles seven.Click
+        ans.Text = ans.Text & 7
+    End Sub
+
+    Private Sub eight_Click(sender As Object, e As EventArgs) Handles eight.Click
+        ans.Text = ans.Text & 8
+    End Sub
+
+    Private Sub nine_Click(sender As Object, e As EventArgs) Handles nine.Click
+        ans.Text = ans.Text & 9
+    End Sub
+
+    Private Sub plus_Click(sender As Object, e As EventArgs) Handles plus.Click
+        value1 = ans.Text
+        ans.Clear()
+        ans.Focus()
+        sign = "+"
+    End Sub
+
+    Private Sub minus_Click(sender As Object, e As EventArgs) Handles minus.Click
+        value1 = ans.Text
+        ans.Clear()
+        ans.Focus()
+        sign = "-"
+    End Sub
+
+    Private Sub multiply_Click(sender As Object, e As EventArgs) Handles multiply.Click
+        value1 = ans.Text
+        ans.Clear()
+        ans.Focus()
+        sign = "x"
+    End Sub
+
+    Private Sub division_Click(sender As Object, e As EventArgs) Handles division.Click
+        value1 = ans.Text
+        ans.Clear()
+        ans.Focus()
+        sign = "/"
+    End Sub
+
+    Private Sub reminder_Click(sender As Object, e As EventArgs) Handles reminder.Click
+        value1 = ans.Text
+        ans.Clear()
+        ans.Focus()
+        sign = "%"
+    End Sub
+
+    Private Sub equals_Click(sender As Object, e As EventArgs) Handles equals.Click
+        value2 = ans.Text
+        If sign = "+" Then
+            ans.Text = value1 + value2
+        ElseIf sign = "-" Then
+            ans.Text = value1 - value2
+        ElseIf sign = "x" Then
+            ans.Text = value1 * value2
+        ElseIf sign = "/" Then
+            ans.Text = value1 / value2
+        ElseIf sign = "%" Then
+            ans.Text = value1 Mod value2
+        End If
+
+    End Sub
+
+    Private Sub point_Click(sender As Object, e As EventArgs) Handles point.Click
+        ans.Text = ans.Text & "."
+    End Sub
+
+    Private Sub back_Click(sender As Object, e As EventArgs) Handles back.Click
+        If ans.Text.Length > 0 Then
+            ans.Text = ans.Text.Remove(ans.Text.Length - 1, 1)
+        End If
+    End Sub
+
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles clear.Click
+        ans.Clear()
+        ans.Focus()
+    End Sub
+End Class
